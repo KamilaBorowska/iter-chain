3bdbef28595a77aec4670db726f1981714b37b80
Konrad Borowski <konrad@borowski.pw>
2017-07-10 22:12:30 +0200

    First commit of iter-chain

---
+++ iter-chain.js
@@ -0,0 +1,81 @@
+"use strict"
+
+const iteratorBase = {
+    map(callback) {
+        return new Map(this, callback)
+    },
+    filter(callback) {
+        return new Filter(this, callback)
+    },
+    sum() {
+        let sum = 0
+        for (const elem of this) {
+            sum += +elem
+        }
+        return sum
+    },
+    return() {
+        return this.iterator.return()
+    },
+    [Symbol.iterator]() {
+        return this
+    },
+}
+
+function getBase() {
+    return Object.create(iteratorBase)
+}
+
+function Map(iterator, callback) {
+    this.iterator = iterator
+    this.callback = callback
+}
+Map.prototype = getBase()
+Map.prototype.next = function () {
+    const {iterator, callback} = this
+    const {done, value} = iterator.next()
+    if (done) {
+        return {done: true}
+    } else {
+        return {value: callback(value), done: false}
+    }
+}
+
+function Filter(iterator, callback) {
+    this.iterator = iterator
+    this.callback = callback
+}
+Filter.prototype = getBase()
+Filter.prototype.next = function () {
+    const {iterator, callback} = this
+    // Cannot use for-of syntax, as this implicitly closes
+    // the iterator.
+    while (true) {
+        const {done, value} = iterator.next()
+        if (done) {
+            return {done: true}
+        }
+        if (callback(value)) {
+            return {value, done: false}
+        }
+    }
+}
+
+function IterChain(iterator) {
+    this.iterator = iterator[Symbol.iterator]()
+}
+IterChain.prototype = Object.create(iteratorBase)
+IterChain.prototype.next = function () {
+    return this.iterator.next()
+}
+IterChain.prototype.return = function () {
+    if (this.iterator.return !== undefined) {
+        return this.iterator.return()
+    }
+}
+
+function iterChain(iterator) {
+    return new IterChain(iterator)
+}
+
+module.exports = iterChain
---
+++ test/test.js
@@ -0,0 +1,64 @@
+const assert = require('assert')
+const iterChain = require('../iter-chain')
+
+describe('iterChain', () => {
+    it('should create iterator identical to original', () => {
+        assert.deepEqual([1, 2, 3], Array.from(iterChain([1, 2, 3])))
+    })
+
+    describe('#map', () => {
+        it('should map all elements', () => {
+            const originalArray = [1, 2, 3]
+            const mapped = iterChain(originalArray).map(x => x * 2)
+            assert.deepEqual(Array.from(mapped), [2, 4, 6])
+        })
+    })
+
+    describe('#filter', () => {
+        it('should filter elements', () => {
+            const originalArray = [1, 2, 3, 4, 5, 6]
+            const filtered = iterChain(originalArray).filter(x => x % 2 !== 0)
+            assert.deepEqual(Array.from(filtered), [1, 3, 5])
+        })
+    })
+
+    describe('#sum', () => {
+        it('should sum elements', () => {
+            const originalArray = [1, 2, 3, 4, 5, 6]
+            assert.deepEqual(iterChain(originalArray).sum(), 1+2+3+4+5+6)
+        })
+    })
+
+    describe('#return', () => {
+        it('should close iterator', () => {
+            let closed = false
+            function* iterator() {
+                try {
+                    while (true) {
+                        yield 42
+                    }
+                } finally {
+                    closed = true
+                }
+            }
+            for (const value of iterChain(iterator())) {
+                break
+            }
+            assert(closed, "iterator's return method not called")
+        })
+    })
+
+    it('should be able to chain multiple functions together', () => {
+        function* range(start, end) {
+            for (let i = start; i <= end; i++) {
+                yield i
+            }
+        }
+        const summed = iterChain(range(1, 1000))
+            .filter(x => x % 2 === 0)
+            .map(x => x * 2)
+            .sum()
+
+        assert.deepEqual(summed, 501000)
+    })
+})
